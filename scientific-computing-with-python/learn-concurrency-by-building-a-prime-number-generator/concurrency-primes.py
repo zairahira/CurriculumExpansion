3381842783ffd0b089f20288b346c5484f557296
zairahira <zairahira@gmail.com>
2023-08-24 13:11:08 +0400

    refactor: prime number generator

--- scientific-computing-with-python/learn-concurrency-by-building-a-prime-number-generator/concurrency-primes.py
+++ scientific-computing-with-python/learn-concurrency-by-building-a-prime-number-generator/concurrency-primes.py
@@ -1,32 +1,74 @@
 import asyncio
+import math 
+import time
 
-async def find_primes(start, end):
+# Define an asynchronous function is_prime(num) that checks whether a given number is prime or not. The function follows the trial division algorithm.
+async def is_prime(num):
+    
+    # Handle base cases: numbers less than or equal to 1 are not prime, and 2 or 3 are prime.
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    
+    # Check divisibility by 2 and 3 to rule out even and multiples of 3.
+    if num % 2 == 0 or num % 3 == 0:
+        return False
 
-    print(f"Finding prime numbers between {start} and {end}.")
+    i = 5
+    # Calculate the integer square root of num using math.isqrt() to determine the upper limit of divisors to check.
+
+    sqrt_num = math.isqrt(num)  # Calculate the integer square root, rounded
+    print("square root of {} is {}".format(num, sqrt_num))
+
+    while i <= sqrt_num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        # Loop through potential divisors in increments of 6, as per the optimization of prime numbers being in the form 6k ± 1.
+
+        i += 6
+
+    return True
+
+
+# Define an asynchronous function generate_primes(limit) that generates a specified number of prime numbers.
+async def generate_primes(limit):
+   
+    # Initialize an empty list primes to store prime numbers.
     primes = []
-    for val in range(start, end + 1):   
-        if val > 1: 
-            for n in range(2, val): 
-                if (val % n) == 0: 
-                    break
-            else: 
-                primes.append(val) 
-                await asyncio.sleep(0.000001)
 
-    print(f"DONE finding prime numbers between {start} and {end}. Total: {len(primes)}")
+    # Initialize num to 2, the first prime number.
+    num = 2
+
+    # Loop until the desired number of prime numbers (limit) is reached.
+    while len(primes) < limit:
+        
+        # Call the is_prime function and await its result to check if num is prime.
+        if await is_prime(num):
+            
+            # If prime, add num to the primes list.
+            primes.append(num)
+
+        # Increment num to check the next number.
+        num += 1
+
     return primes
 
 
+# Define the main() async function, which serves as the entry point for the asynchronous program.
 async def main():
-    primes1 = loop.create_task(find_primes(100, 10000))
-    primes2 = loop.create_task(find_primes(10, 100))
-    primes3 = loop.create_task(find_primes(1, 10))
-    await asyncio.wait([primes1,primes2,primes3])
-    return primes1, primes2, primes3
+    
+    # Set the desired number of prime numbers to generate (limit).
+    limit = 1000  
+    
+    # Call the generate_primes function with the specified limit and await the result.
+    primes = await generate_primes(limit)
 
+    print(f"The first {limit} prime numbers are: {primes}")
+    t1 = time.time()
+    print("execution time for async: ", t1-t0)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    primes1, primes2, primes3 = loop.run_until_complete(main())
-    print(primes3.result())
-    loop.close()
+    
+    # Set up the asyncio event loop and execute the asynchronous tasks within the main() function.
+    asyncio.run(main())
